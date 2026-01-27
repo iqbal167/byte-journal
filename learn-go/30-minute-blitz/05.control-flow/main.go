d8e307c4aafb8eec0a19af26baf3eb7587d6f775
Muhammad Iqbal Ali <muhammadiqbalali167@gmail.com>
2026-01-27 14:07:57 +0700

    feat: add control flow tutorial with examples
    Introduce a new tutorial module covering Go control flow constructs.
    Includes examples of if-else, switch, for loops with break/continue,
    and demonstrations of rarely used features like fallthrough and goto.

---
+++ learn-go/30-minute-blitz/05.control-flow/main.go
@@ -0,0 +1,82 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num1 int = 10
+
+	// if-else statement
+	if num1 > 0 {
+		fmt.Println("num1 is positive")
+	} else if num1 < 0 {
+		fmt.Println("num1 is negative")
+	} else {
+		fmt.Println("num1 is zero")
+	}
+
+	// switch statement
+	switch num1 {
+	case 1:
+		fmt.Println("num1 is one")
+	case 2:
+		fmt.Println("num1 is two")
+	default:
+		fmt.Println("num1 is neither one nor two")
+	}
+
+	for i := range 10 {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+
+	// for with braak
+	for i := range 10 {
+		if i == 5 {
+			break
+		}
+		fmt.Println(i)
+	}
+
+	// for with continue
+	for i := range 10 {
+		if i%2 == 0 {
+			continue
+		}
+		fmt.Println(i)
+	}
+
+	// switch statement with fallthrough: fallthrough is rarely used in Go
+	switch num1 {
+	case 1:
+		fmt.Println("num1 is one")
+		// fallthrough to the next case
+		fallthrough
+	case 2:
+		fmt.Println("num1 is two")
+	default:
+		fmt.Println("num1 is neither one nor two")
+	}
+
+	// switch with goto: goto is rarely used in Go
+	switch num1 {
+	case 1:
+		fmt.Println("num1 is one")
+		goto end
+	case 2:
+		fmt.Println("num1 is two")
+	default:
+		fmt.Println("num1 is neither one nor two")
+	}
+
+	// if with goto
+	for i := range 10 {
+		if i == 5 {
+			goto end
+		}
+		fmt.Println(i)
+	}
+
+end:
+	fmt.Println("end of switch statement")
+}
