68c2a7da738b6035982702504afd1ff547dbc89c
Muhammad Iqbal Ali <muhammadiqbalali167@gmail.com>
2026-01-28 18:09:57 +0700

    chore: ignore tasks.json and add storage module for task persistence
    Add tasks.json to .gitignore to avoid committing generated data file.
    Introduce storage.py with functions to load and save task state to JSON,
    ensuring a default structure is returned if the file is missing or invalid.

---
+++ build-something/project/task-tracker/storage.py
@@ -0,0 +1,28 @@
+import json
+
+FILE_NAME = "tasks.json"
+
+def get_default_structure():
+    return { "tasks": [] }
+
+def load_state():
+    default_structure = get_default_structure()
+    try:
+        with open(FILE_NAME, "r") as file:
+            data =  json.load(file)
+
+            # Validate the loaded data structure   
+            # isinstance is used to check if the data is a dictionary and if it has a "tasks" key with a list value
+            if isinstance(data, dict) and "tasks" in data and isinstance(data["tasks"], list):
+                return data
+
+            return default_structure
+
+    except FileNotFoundError:
+        return default_structure
+    except json.JSONDecodeError:
+        return default_structure
+
+def save_state(data):
+    with open(FILE_NAME, "w") as file:
+        json.dump(data, file, indent=4)
